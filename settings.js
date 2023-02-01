8eeb5b0c018749500d2670246765f935d9a3d4ba
Abdullah <121610160+abdullahwy@users.noreply.github.com>
2023-02-01 15:13:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 
 //documents variants
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['94771966060'] //ur owner number
+global.ownername = "ArcX Official" //ur owner name
+global.ytname = "YouTube : ArcX Official" //ur yt chanel name
+global.socialm = "GitHub: ArcX Official" //ur github or insta name
+global.location = "Sri Lanka, Matara, Matara" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
-global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
-global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.ownernomer = "94771966060" //ur number
+global.premium = ['94772582662','94704281955'] //ur premium number
+global.botname = 'ArcX Whatsapp Bot' //ur bot name
+global.linkz = "https://chat.whatsapp.com/HWztD2uShNqFbXhzVNRh9S" //your theme url which will be displayed on whatsapp
+global.websitex = "https://wa.me/94771966060" //ur website to be displayed
+global.botscript = 'https://github.com/abdullahwy/Black-Dragon' //script link
+global.themeemoji = "😎" //ur theme emoji
+global.packname = "Sticker By ArcX md sticker pack" //ur sticker watermark packname
+global.author = "ArcX Official" //ur sticker watermark author
+global.wm = "ArcX Official INC" //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -73,11 +73,11 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 }
    global.rpg = {
    darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
+   besiawal: 100,
+   goldawal: 100,
+   emeraldawal: 100,
+   umpanawal: 100,
+   potionawal: 100
 }
 
 global.limitAwal = {
